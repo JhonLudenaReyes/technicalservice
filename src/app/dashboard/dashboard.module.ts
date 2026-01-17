200f79d582a80c77398ab460e504268d36dbf2e8
JhonLudenaReyes <ludena.jhon.j.6392@outlook.com>
2026-01-16 22:26:04 -0500

    Dashboard Working Sidebar

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { AdminComponent } from './components/admin/admin.component';
 import { MainComponent } from './components/main/main.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
+import { SidebarModule } from 'src/app/sidebar/sidebar.module';
 
 @NgModule({
   declarations: [AdminComponent, MainComponent],
-  imports: [CommonModule, DashboardRoutingModule],
+  imports: [CommonModule, DashboardRoutingModule, SidebarModule],
 })
 export class DashboardModule {}
